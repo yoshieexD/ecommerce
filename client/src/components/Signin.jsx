4130fee201f18d9dca0be68d7c200aa54a09d576
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-08 17:56:23 +0800

    login

---
+++ client/src/components/Signin.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/Signin.css'
+const Signin = () => {
+    return (
+        <div className='container'>
+            <form>
+                <div className="form-floating">
+                    <input type="email" className="form-control form-control-lg" id="floatingInput" placeholder="name@example.com" />
+                    <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <br />
+                <div className="form-floating mb-3">
+                    <input type="password" className="form-control form-control-lg" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <button type="submit" className="btn btn-primary btn-lg mt-2">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Signin;
--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Signin from '../components/Signin';
 
 const Index = () => {
     return (
         <div>
+            <Signin />
         </div>
     );
 };
