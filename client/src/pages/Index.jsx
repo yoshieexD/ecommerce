ec18ded5fc5a02db480cea254074a8ada477983c
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-08 16:20:41 +0800

    material ui not stable

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { useState } from 'react';
-import { Paper, Chip } from '@mui/material';
-import '../app.css'
+
 const Index = () => {
-    const [checked, setChecked] = useState(false);
-
-    const handleChange = (event) => {
-        setChecked(event.target.checked)
-    };
-
     return (
         <div>
-            <Paper elevation={3}>
-                <Chip label="login" color="primary"
-                    variant='outlined' />
-            </Paper>
+            asd
         </div>
     );
 };
