434f3f07fb2af55d4d6e68aed3e970396fd029cc
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-08 15:14:36 +0800

    initial development

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import '../css/index.css'
 const Index = () => {
     return (
-        <div>
-            Testing
+        <div className="login-container">
+            <h2>Login</h2>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" placeholder="Enter your email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" placeholder="Enter your password" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 };
