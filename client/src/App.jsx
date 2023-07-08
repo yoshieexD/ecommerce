bdc08f957393f2073b88355b3923c2ac10c504d9
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-08 17:15:05 +0800

    bootstrap added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Index from './pages/Index'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js'
 function App() {
 
   return (
--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Index = () => {
     return (
         <div>
-            asasdasdsasdsa
         </div>
     );
 };
