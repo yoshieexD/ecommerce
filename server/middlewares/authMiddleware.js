b6ed9ef09c16aedf0d79a566dbb55846885f65df
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-15 01:04:52 +0800

    admin role & protected route

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,12 +1,39 @@
-import JWT from 'jsonwebtoken'
+import JWT from 'jsonwebtoken';
+import userModel from '../models/userModel.js';
 
-//Protected routes using token
+// Protected routes using token
 
 export const requireLogin = async (req, res, next) => {
     try {
         const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET);
+        req.user = decode;
         next();
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        res.status(401).send({
+            success: false,
+            message: 'Unauthorized',
+        });
     }
-}
+};
+
+export const isAdmin = async (req, res, next) => {
+    try {
+        const user = await userModel.findById(req.user._id);
+        if (user.role !== 1) {
+            return res.status(401).send({
+                success: false,
+                message: 'Unauthorized access',
+            })
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({
+            success: false,
+            error,
+            messsage: 'error in admin middleware',
+        })
+    }
+};
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { registerController, loginController, testController } from "../controllers/authController.js";
-import { requireLogin } from "../middlewares/authMiddleware.js";
+import { isAdmin, requireLogin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/register', registerController);
 router.post('/login', loginController);
-router.get('/test', requireLogin, testController)
+router.get('/test', requireLogin, isAdmin, testController)
 
 export default router;
