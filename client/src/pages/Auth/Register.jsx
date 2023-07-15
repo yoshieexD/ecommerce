820bbbd8476e3706dc5e180e70cbf07597301792
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-16 00:12:38 +0800

    testing

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const res = await axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/auth/register`, { name, email, phone, address, password })
+            const res = await axios.post(`https://ecommerce-yoshieexd.vercel.app/api/v1/auth/register`, { name, email, phone, address, password })
             if (res.data.success) {
                 toast.success(res.data.message);
                 navigate('/login');
