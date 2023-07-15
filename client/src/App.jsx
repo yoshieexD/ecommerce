d9741c0d553c4237f299d058bd666bc2ec27d87c
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-15 16:29:49 +0800

    navbar link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Index from './pages/Index'
+import Index from './pages/Index';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/js/bootstrap.js';
+import './index.css';
 function App() {
 
   return (
@@ -10,6 +15,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Index />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/policy' element={<Policy />} />
+          <Route path='*' element={<Pagenotfound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-light p-4'>
+            <h4 className='text-center'>
+                All Right Reserved &copy; Jom
+            </h4>
+        </div>
+    );
+};
+
+export default Footer
