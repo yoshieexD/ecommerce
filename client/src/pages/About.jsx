62687edc0b6a5f4df61142130a0763764709850c
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-15 20:18:03 +0800

    about and contact page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import aboutus from '../assets/aboutus.jpg'
 
 const About = () => {
     return (
         <Layout>
-            <h1>About Page</h1>
+            <div className='row contactus d-flex justify-content-center align-items-center m-auto'>
+                <div className='col-md-6 mt-3'>
+                    <img src={aboutus} style={{ width: "100%" }} alt="Contact Us" />
+                </div>
+                <div className='col-md-4'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim diam vulputate ut pharetra sit amet aliquam. Tempor commodo ullamcorper a lacus vestibulum sed arcu. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Nunc vel risus commodo viverra maecenas. Dictum fusce ut placerat orci nulla pellentesque dignissim enim. Ac tortor vitae purus faucibus ornare suspendisse sed nisi. Diam phasellus vestibulum lorem sed risus ultricies tristique. Nunc sed blandit libero volutpat sed. Consequat ac felis donec et odio pellentesque diam volutpat. Sit amet luctus venenatis lectus magna fringilla. Amet facilisis magna etiam tempor. Nec ullamcorper sit amet risus nullam eget felis. Aliquam id diam maecenas ultricies mi eget mauris pharetra. Dictum varius duis at consectetur lorem donec massa sapien.</p>
+                </div>
+            </div>
         </Layout>
     );
 };
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { BiMailSend, BiPhoneCall, BiSupport } from 'react-icons/bi'
+import { BiMailSend, BiPhoneCall, BiSupport } from 'react-icons/bi';
+import contactus from '../assets/contactus.jpg';
+
 const Contact = () => {
     return (
         <Layout>
-            <div></div>
+            <div className='row contactus d-flex justify-content-center align-items-center m-auto'>
+                <div className='col-md-6 mt-3'>
+                    <img src={contactus} style={{ width: "100%" }} alt="Contact Us" />
+                </div>
+                <div className='col-md-4'>
+                    <h1 className="bg-dark p-2 text-white text-center">Contact US</h1>
+                    <p className="text-justify mt-2">
+                        any query and info about the product feel free to call anytime we are 24 hours open
+                    </p>
+                    <p className="mt-3">
+                        <BiMailSend /> : www.sample@sample.com
+                    </p>
+                    <p className='mt-3'>
+                        <BiPhoneCall /> : +63-1233-123
+                    </p>
+                    <p className="mt-3">
+                        <BiSupport /> : 1434-23435
+                    </p>
+                </div>
+            </div>
         </Layout>
     );
 };
 
 export default Contact;
+
