50bfde309af3e0b9a4159ddd85ca4e643e4ffd15
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-17 14:41:33 +0800

    http to https

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 // Configure env
 dotenv.config();
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://ecommerce-front-navy.vercel.app'],
+    origin: ['http://localhost:3000', 'https://ecommerce-front-navy.vercel.app'],
     methods: ["GET", "POST"],
 };
 
