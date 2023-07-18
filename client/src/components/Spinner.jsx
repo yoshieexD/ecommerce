0eecf7dfed10066e085b6deace4290c6fe8c6d54
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-19 01:02:48 +0800

    redirect

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Spinner = () => {
+    const [count, setCount] = useState(5);
+    const navigate = useNavigate();
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((prevValue) => --prevValue)
+        }, 1000)
+        count === 0 && navigate('/login')
+        return () => clearInterval(interval)
+    }, [count, navigate])
     return (
-        <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <div className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+            <h1 className='text-center'>redirecting to you in {count} second</h1>
             <div className="spinner-border text-primary" role="status">
                 <span className="sr-only"></span>
             </div>
