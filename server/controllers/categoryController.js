9eec0b74e520c1189b859ba3d16525c1af1ae8f0
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-22 21:51:49 +0800

    delete by id delete, update

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -67,3 +67,40 @@ export const categoryController = async (req, res) => {
 
     }
 };
+
+export const singleCategoryController = async (req, res) => {
+    try {
+        const category = await categoryModel.findOne({ slug: req.params.slug })
+        res.status(200).send({
+            success: true,
+            message: 'Get single category successfully',
+            category
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error while getting Single Category'
+        })
+    }
+};
+
+export const deleteCategoryController = async (req, res) => {
+    try {
+        const { id } = req.params
+        await categoryModel.findByIdAndDelete(id);
+        res.status(200).send({
+            success: true,
+            message: "Category deleted successfully"
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'error while deleting the category',
+            error
+        })
+    }
+
+};
--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAdmin, requireLogin } from '../middlewares/authMiddleware.js'
-import { categoryController, createCategoryController, updateCategoryController } from '../controllers/categoryController.js'
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from '../controllers/categoryController.js'
 
 const router = express.Router();
 
@@ -8,7 +8,10 @@ router.post('/create-category', requireLogin, isAdmin, createCategoryController)
 
 //update
 router.put('/update-category/:id', requireLogin, isAdmin, updateCategoryController);
+//get all
 router.get('/get-category', categoryController);
-
+//single 
+router.get('/single-category/:slug', singleCategoryController);
+router.delete('/delete-category/:id', requireLogin, isAdmin, deleteCategoryController);
 
 export default router;
